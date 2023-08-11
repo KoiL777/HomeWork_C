8a05148877ce46aeb9eb7e7d79a675b8e068997a
Alex KoiL <leshka20091@yandex.ru>
2023-08-11 15:30:03 +0300

    task54 ready

---
+++ Task54/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int rows = 3;
+int colums = 4;
+
+int[,] CreateMatrixyRndInt(int rows, int colums, int min, int max) 
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+void SortedRowsMatrix(int[,] matrix)
+{
+    for (int m = 0; m < matrix.GetLength(0); m++)
+    {
+        for (int n = 0; n < matrix.GetLength(1); n++)
+        {
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    if (matrix[i, j] > matrix[i, j + 1])
+                    {
+                        int temp = matrix[i, j];
+                        matrix[i, j] = matrix[i, j + 1];
+                        matrix[i, j + 1] = temp;
+                    }
+                }
+            }
+
+        }
+    }
+
+}
+Console.WriteLine();
+int[,] array2d = CreateMatrixyRndInt(rows, colums, 1, 20);
+PrintMatrix(array2d);
+Console.WriteLine();
+
+Console.WriteLine("Упорядоченный массив: ");
+
+Console.WriteLine();
+SortedRowsMatrix(array2d);
+PrintMatrix(array2d);
+Console.WriteLine();
