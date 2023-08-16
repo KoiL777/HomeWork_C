a20b490c0cd6a826af3f552741a148a636032ee5
Alex KoiL <leshka20091@yandex.ru>
2023-08-16 11:06:12 +0300

    task66 ready

---
+++ Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите натуральное число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n < 0 || m < 0)
+{
+    Console.WriteLine("Ошибка! Не корректное число!");
+    return;
+}
+int NumbersRange(int n, int m)
+{
+
+    if (n == m) return n;
+    if (n > m) return NumbersRange(n - 1, m) + n;
+    else return NumbersRange(n + 1, m) + n;
+
+}
+Console.WriteLine($"Сумма чисел диапозона от {n} до {m} равна {NumbersRange(n, m)}");
